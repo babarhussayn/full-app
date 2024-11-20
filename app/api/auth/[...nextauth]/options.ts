c018a941fdb0215032a6d94ac05026f008d030e0
babarhussain <babarhusain262@gmail.com>
2024-11-20 10:10:21 +0500

    auth provider options

--- app/api/auth/[...nextauth]/options.ts
+++ app/api/auth/[...nextauth]/options.ts
@@ -1,5 +1,37 @@
 import type { NextAuthOptions } from "next-auth";
-
+import GitHubProvider from "next-auth/providers/github";
+import CredentialsProvider from "next-auth/providers/credentials";
 export const options: NextAuthOptions = {
-  providers: [],
+  providers: [
+    GitHubProvider({
+      clientId: process.env.GITHUB_ID as string,
+      clientSecret: process.env.GITHUB_SECRET as string,
+    }),
+    CredentialsProvider({
+      name: "Credential",
+      credentials: {
+        username: {
+          label: "username",
+          type: "text",
+          placeholder: "Username",
+        },
+        password: {
+          label: "password",
+          type: "password",
+          placeholder: "password",
+        },
+      },
+      async authorize(credentials) {
+        const user = { id: "2", name: "babar", password: "babar123" };
+        if (
+          credentials.username === user.name &&
+          credentials.password === user.password
+        ) {
+          return user;
+        } else {
+          return null;
+        }
+      },
+    }),
+  ],
 };
